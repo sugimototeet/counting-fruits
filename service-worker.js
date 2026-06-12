c47c91a65afbfc152b7bb4ef1d88b66c4de5af99
sugimototeet <sugimototeet8810@gmail.com>
2026-06-12 18:09:48 +0900

    Add service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "counting-fruits-v1";
+const APP_FILES = [
+  "./",
+  "index.html",
+  "manifest.json",
+  "icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
